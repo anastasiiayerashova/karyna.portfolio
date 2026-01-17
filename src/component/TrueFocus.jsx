d0d44ab33dbafe3dca1e30a6de79e32ae81ae301
Anastasiia Yerashova <yerashova.a@gmail.com>
2026-01-17 16:19:00 +0100

    fix true focus

--- src/component/TrueFocus.jsx
+++ src/component/TrueFocus.jsx
@@ -18,7 +18,14 @@ const TrueFocus = ({
   const containerRef = useRef(null);
   const wordRefs = useRef([]);
   const [focusRect, setFocusRect] = useState({ x: 0, y: 0, width: 0, height: 0 });
-
+    
+  useEffect(() => {
+    return () => {
+      setCurrentIndex(-1)
+      setFocusRect({ x: 0, y: 0, width: 0, height: 0 })
+    }
+  }, [])
+    
   useEffect(() => {
     if (!manualMode) {
       const interval = setInterval(
