a655f5d46aee76a03e86e1a784af7dc03bcddfb8
Anastasiia Yerashova <yerashova.a@gmail.com>
2026-01-17 14:12:34 +0100

    change colors in menu

--- src/component/StaggeredMenu.jsx
+++ src/component/StaggeredMenu.jsx
@@ -4,7 +4,7 @@ import './StaggeredMenu.css';
 
 export const StaggeredMenu = ({
   position = 'right',
-  colors = ['#B19EEF', '#5227FF'],
+  colors = ['#fff', '#a45933'],
   items = [],
   socialItems = [],
   displaySocials = true,
